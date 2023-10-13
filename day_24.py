8086b9a9bfc22eaba23f71fe0595495511e8fc87
Gravitar64 <gravitar@web.de>
2023-10-13 11:51:57 +0200

    V1 1,94 Sek.

--- day_24.py
+++ day_24.py
@@ -1,106 +1,98 @@
 import time
 import re
 
-ID, NAME, UNITS, HP, DAMAGE, INIT, DTYPE, IMMUNE, WEAK = range(9)
-
-class Group:
-  def __init__(self,id,army,units,hp,damage,init,dtype,immune,weak):
-    self.id = id
-    self.army = army
-    self.units = units
-    self.hp = hp
-    self.damage = damage
-    self.dtype = dtype
-    self.immune = immune
-    self.weak = weak
-    self.init = init
-    self.power = units * damage
-    self.selected = False
-    
-
-  def damage_to(self, defender):
-    if self.dtype in defender.immune: return 0
-    if self.dtype in defender.weak: return self.power * 2
-    return self.power
-
-  def attacked_by(self, attacker):
-    damage = attacker.damage_to(self)
-    kills = damage // self.hp
-    self.units = max(0, self.units - kills)
-    self.power = self.units * self.damage
-    return kills == 0
-    
 
 def load(file):
   with open(file) as f:
     return [[group for group in block.split('\n')] for block in f.read().split('\n\n')]
-  
+
 
 def parse(p):
   groups = []
   for army in p:
     name = army[0][:-1]
-    for i,group in enumerate(army[1:],start=1):
-      numbers = map(int,re.findall('\d+',group))
-      dtype = re.findall('(\w+) damage',group)[0]
-      weak = re.findall(r'weak to ([a-z, ]*){?|\)}',group)
-      if weak: weak = weak[0].split(', ')
-      immune = re.findall(r'immune to ([a-z, ]*){?|\)}',group)
-      if immune: immune = immune[0].split(', ')
-      groups.append(Group(i, name, *numbers, dtype, immune, weak))
+    for i, group in enumerate(army[1:], start=1):
+      numbers = map(int, re.findall('\d+', group))
+      dtype = re.findall('(\w+) damage', group)[0]
+      weak = re.findall(r'weak to ([a-z, ]*){?|\)}', group)
+      if weak: 
+        weak = set(weak[0].split(', '))
+      immune = re.findall(r'immune to ([a-z, ]*){?|\)}', group)
+      if immune: 
+        immune = set(immune[0].split(', '))
+      groups.append([i, name, *numbers, dtype, immune, weak, False])
   return groups
 
 
+def real_damage(enemie, defender):
+  if enemie[NAME] == defender[NAME]: return 0
+  if enemie[DTYPE] in defender[IMMUNE]: return 0
+  if enemie[DTYPE] in defender[WEAK]:  return enemie[UNITS] * enemie[DAMAGE] * 2
+  return enemie[UNITS] * enemie[DAMAGE]
+
+
 def target_selection(groups):
-  groups = sorted(groups,key=lambda x: (-x.power,-x.init))
-  pairs = []
-  for attacker in groups:
-    remaining_targets = [g for g in groups if attacker.army != g.army and not g.selected]
+  battles = []
+  for enemie in sorted(groups, key=lambda g: (-g[UNITS] * g[DAMAGE], -g[INIT])):
+    remaining_targets = [g for g in groups if not g[SELECT] and real_damage(enemie, g)]
     if not remaining_targets: continue
-    defender = max(remaining_targets, key=lambda g: (attacker.damage_to(g), g.power, g.init))
-    defender.selected = True
-    pairs.append((attacker,defender))
-  return pairs  
+    defender = max(remaining_targets, key=lambda g: (real_damage(enemie, g), g[UNITS] * g[DAMAGE], g[INIT]))
+    defender[SELECT] = True
+    battles.append((enemie, defender))
+  return battles
+
+
+def attack(enemie, defender):
+  damage = real_damage(enemie, defender)
+  kills = min(defender[UNITS], damage // defender[HP])
+  defender[UNITS] -= kills
+  return kills
 
 
 def solve(p):
+  #part1
   groups = parse(p)
-  
   while True:
     pairs = target_selection(groups)
-    pairs = sorted(pairs,key=lambda x: -x[0].init)
-    for attacker, defender in pairs:
-      defender.attacked_by(attacker)
+    for enemie, defender in sorted(pairs, key=lambda pair: -pair[0][INIT]):
+      attack(enemie, defender)
     
-    groups = [group for group in groups if group.units > 0]
-    for g in groups: g.selected = False          
-    armies = {g.army for g in groups}
-    if len(armies) == 1:
-      part1 =  sum(g.units for g in groups)
+    groups = [g for g in groups if g[UNITS] > 0]
+    
+    for g in groups:
+      g[SELECT] = False
+    
+    if len(set([g[NAME] for g in groups])) == 1:
+      part1 = sum(g[UNITS] for g in groups)
       break
 
+  #part2
   for boost in range(2000):
     groups = parse(p)
-    for g in groups: 
-      if g.army == 'Immune System': 
-        g.damage += boost
-        g.power = g.units * g.damage
-    
+    for g in groups:
+      if g[NAME] == 'Infection': continue
+      g[DAMAGE] += boost
     while True:
       pairs = target_selection(groups)
-      pairs = sorted(pairs,key=lambda x: -x[0].init)
-      kills = [defender.attacked_by(attacker) for attacker, defender in pairs]
+      attacking_sequenz = sorted(pairs, key=lambda pair: -pair[0][INIT])
+      kills = [attack(enemie, defender) == 0 for enemie,
+               defender in attacking_sequenz]
+      
       if all(kills): break
-      groups = [group for group in groups if group.units > 0]
-      for g in groups: g.selected = False          
-      armies = {g.army for g in groups}
-      if armies == {'Infection'}: break  
-      if armies == {'Immune System'}: 
-        print(boost)
-        return part1, sum(g.units for g in groups)
-        
+
+      groups = [g for g in groups if g[UNITS] > 0]
+      for g in groups:
+        g[SELECT] = False
+
+      armies = set([g[NAME] for g in groups])
+      if armies == {'Immune System'}:
+        return part1, sum(g[UNITS] for g in groups)
+      if armies == {'Infection'}:
+        break
+
 
 start = time.perf_counter()
+ID, NAME, UNITS, HP, DAMAGE, INIT, DTYPE, IMMUNE, WEAK, SELECT = range(10)
 puzzle = load('day_24.txt')
 part1, part2 = solve(puzzle)
 print(f'Part1: {part1}')
